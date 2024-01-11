3ddc4d89893e8c6f4b5cdb077c30d70d6a42bc7e
MishaHoekstra06 <misha.hoekstra@student.nhlstenden.com>
2024-01-11 10:11:04 +0100

    MQTT werking for real

--- arduinoprojectgithub.ino
+++ arduinoprojectgithub.ino
@@ -116,7 +116,7 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly
 
     unsigned long currentMillis = millis();
 
